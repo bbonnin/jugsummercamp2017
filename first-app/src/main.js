7b2a720a709d77025e1e7e2c275f205d5ddc0ad5
Bruno Bonnin <bbonnin@gmail.com>
2017-09-10 18:16:04 +0200

    Update first-app

--- first-app/src/main.js
+++ first-app/src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 import store from './store'
 
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  store,
-  render: h => h(App)
+  template: '<App/>',
+  components: { App },
+  store
 })
